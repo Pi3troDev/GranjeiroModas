b41294fc886f571eb5c97949d954d00ead3f94ba
Pietro <pietroalves.dev@gmail.com>
2025-07-24 20:15:21 -0300

    Commit Granjeiro Modas

---
+++ script.js
@@ -0,0 +1,13 @@
+const carrossel = document.getElementById("carrossel");
+const btnEsquerda = document.getElementById("seta-esquerda");
+const btnDireita = document.getElementById("seta-direita");
+
+const scrollValor = 300; // distância a rolar por clique
+
+btnDireita.addEventListener("click", () => {
+  carrossel.scrollBy({ left: scrollValor, behavior: "smooth" });
+});
+
+btnEsquerda.addEventListener("click", () => {
+  carrossel.scrollBy({ left: -scrollValor, behavior: "smooth" });
+});
